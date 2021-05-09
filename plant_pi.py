c803943377503c8b7a852fc26e252502069dfcae
Alexander Becker <nabla.becker@mailbox.org>
2021-05-09 11:57:07 +0100

    some minor formatting tweaks

--- plant_pi.py
+++ plant_pi.py
@@ -10,16 +10,19 @@ if !i2c.try_lock():
     print("Could not aquire exclusive use of the i2c")
     exit(1)
 
+for plant in plants:
+    print("Found plantpot at: {}".format(plant))
+
 while True:
     for plant in plants:
         buf = bytearray(8)
         print("Reading from plant {}".format(plant))
-        i2c.readfrom_into(plant, buf, 0, 8)
+        i2c.readfrom_into(plant, buf, start=0, end=8)
         raw_moist = buf[:4]
         raw_bright = buf[4:]
-        moisture = struct.unpack('f', raw_moist)
-        brightness = struct.unpack('f', raw_bright)
+        moisture = struct.unpack('f', raw_moist)[0]
+        brightness = struct.unpack('f', raw_bright)[0]
         print("Moisture: {}".format(moisture))
         print("Brightness: {}".format(brightness))
-        sleep(1)
+        time.sleep(1)
               
