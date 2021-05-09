4f827ea864cd906b7627a8a662ba0cacb4d02344
Alexander Becker <nabla.becker@mailbox.org>
2021-05-09 11:48:33 +0100

    changed minor problems with the python client

--- plant_pi.py
+++ plant_pi.py
@@ -2,9 +2,8 @@ import time
 import struct
 import board
 import busio
-import _typing
 
-i2c = busio.I2C(board.SCL, board.SDA)
+i2c = busio.I2C(board.SCL, board.SDA, 100000)
 
 plants = i2c.scan()
 if !i2c.try_lock():
@@ -14,12 +13,13 @@ if !i2c.try_lock():
 while True:
     for plant in plants:
         buf = bytearray(8)
-        print("Reading from plant {}".format(plant)
+        print("Reading from plant {}".format(plant))
         i2c.readfrom_into(plant, buf, 0, 8)
-        raw_moist = buf(range(4))
-        raw_bright = buf(range(4, 8))
+        raw_moist = buf[:4]
+        raw_bright = buf[4:]
         moisture = struct.unpack('f', raw_moist)
         brightness = struct.unpack('f', raw_bright)
         print("Moisture: {}".format(moisture))
         print("Brightness: {}".format(brightness))
-
+        sleep(1)
+              
