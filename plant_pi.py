3c81d94997d71d62fdff267d1663143568fb5947
Alexande Becker <becker.alexander42@gmail.com>
2021-05-09 12:31:52 +0200

    added sample code that still needs testing

--- plant_pi.py
+++ plant_pi.py
@@ -1,11 +1,25 @@
 import time
+import struct
 import board
 import busio
-import adafruit_bme280
+import _typing
+
 i2c = busio.I2C(board.SCL, board.SDA)
-bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+
+plants = i2c.scan()
+if !i2c.try_lock():
+    print("Could not aquire exclusive use of the i2c")
+    exit(1)
+
 while True:
-    print("\nTemperature: {:.1f} C".format(bme280.temperature))
-    print("Humidity: {:.1f} %%".format(bme280.humidity))
-    print("Pressure: {:.1f} hPa".format(bme280.pressure))
-    time.sleep(2)
+    for plant in plants:
+        buf = bytearray(8)
+        print("Reading from plant {}".format(plant)
+        i2c.readfrom_into(plant, buf, 0, 8)
+        raw_moist = buf(range(4))
+        raw_bright = buf(range(4, 8))
+        moisture = struct.unpack('f', raw_moist)
+        brightness = struct.unpack('f', raw_bright)
+        print("Moisture: {}".format(moisture))
+        print("Brightness: {}".format(brightness))
+
